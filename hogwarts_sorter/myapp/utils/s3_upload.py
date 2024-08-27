f41ac4d95bdb9479ee0c7dc90f67e5185bf0e6e1
howardhoho <b04504029@gmail.com>
2024-08-26 22:55:20 -0500

    Update s3 upload

--- hogwarts_sorter/myapp/utils/s3_upload.py
+++ hogwarts_sorter/myapp/utils/s3_upload.py
@@ -1,11 +1,13 @@
 import boto3
 import io
+from PIL import Image
+import numpy as np
 
 # Initialize S3 client
 s3 = boto3.client('s3')
 
 # Your bucket name
-BUCKET_NAME = 'your-bucket-name'
+BUCKET_NAME = 'sorting-hat-howard'
 
 def upload_image_to_s3(image, filename, file_format):
     """Uploads an image to an S3 bucket in the original format."""
@@ -17,7 +19,15 @@ def upload_image_to_s3(image, filename, file_format):
 
     try:
         s3.upload_fileobj(img_byte_arr, BUCKET_NAME, s3_file_name)
+        print("successfully uploaded!")
     except Exception as e:
         raise RuntimeError(f"Failed to upload image to S3: {e}")
     
+    
     return s3_file_name
+
+
+
+    
+
+    
